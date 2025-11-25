6103323eb7b92f859e2f097c331ed151b0f6dfd6
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-11-25 14:17:19 -0500

    added config and dotenv package

--- config/config.go
+++ config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBURI string
+}
+
+func LoadConfig() (Config, error) {
+	var err error
+	env := os.Getenv("WT_APP_ENV")
+	if env == "local" {
+		err = godotenv.Load(".env.local")
+	} else {
+		err = godotenv.Load(".env.prod")
+	}
+	config := Config{
+		DBURI: os.Getenv("DB_URI"),
+	}
+	return config, err
+}
